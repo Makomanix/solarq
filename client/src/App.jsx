2194eaf1642e0d15815a5f0ff4b51006af2a1193
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-14 15:10:24 -0500

    added reactrouterdom/tailwind, removed test files

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Page Count: {count}</h1>
+    </div>
+  );
+}
+
+export default App;
