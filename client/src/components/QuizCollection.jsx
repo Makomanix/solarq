d4c6962116ca473cc134f61f06cbfd63384bc9b7
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-19 23:39:31 -0500

    fixed unknown seed error

--- client/src/components/QuizCollection.jsx
+++ client/src/components/QuizCollection.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Question from './Question';
 
 export default function QuizCollection({questions}) {
+    const [quiz, setQuiz] = useState([])
 
     const questionCards = questions.map((question) =>
         <Question 
@@ -11,19 +12,20 @@ export default function QuizCollection({questions}) {
             question={question}
             />)
 
-    const [quiz, setQuiz] = useState([])
-    
+    const selectedQuiz = questions.filter((question) => question.catagory === `${quiz}`)
     
     const handleQuizClick = (e) => {
-        setQuiz(`${e.target.value}`)
+        setQuiz(e.target.value)
     }
+
+    console.log(selectedQuiz)
 
     return (
         <div>
             <div className='grid grid-cols-3 grid-rows-1'>
-                <button value="isPlanet" onClick={handleQuizClick}>Planet Quiz</button>
-                <button value="isMoon" onClick={handleQuizClick}>Moon Quiz</button>
-                <button value="isOther" onClick={handleQuizClick}>Sun and Other Quiz</button>
+                <button value="planet" onClick={handleQuizClick}>Planet Quiz</button>
+                <button value="moon" onClick={handleQuizClick}>Moon Quiz</button>
+                <button value="other" onClick={handleQuizClick}>Sun and Other Quiz</button>
             </div>
             {/* <Quiz currentQuiz={quiz}/> */}
         </div>
