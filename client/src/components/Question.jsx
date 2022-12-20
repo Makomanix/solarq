c4bb6c51d4baac507b19517d2236534111cd6943
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-20 11:09:10 -0500

    saving before merge to main

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 export default function Question({question}) {
     const { text, difficulty, points, catagory, answer, option1, option2, option3, option4, hint } = question
+
+    const handleClick = () => {
+
+    }
+
+
     return (
-        <div>Question</div>
+        <div>
+            <div></div>
+
+        </div>
     )
 }
